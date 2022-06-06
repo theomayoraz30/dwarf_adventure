3d10aa7356639b3681b57662fc147d097543accb
Nathan Antonietti <nathan4567@outlook.com>
2022-06-06 22:30:36 +0200

    Mouvement et attaque

--- player/player.gd
+++ player/player.gd
@@ -2,12 +2,23 @@ extends KinematicBody2D
 
 # Speed for the player
 export (int) var SPEED = 200;
+export (int) var MAX_HEALTH = 6; # 1 heart = 2 hps
+export (int) var health = 6; # 1 heart = 2 hps
 
 var velocity = Vector2();
-	
-func _physics_process(delta):
+
+# this will execute often
+func _physics_process(_delta):
 	var x = Input.get_axis("left", "right");
 	var y = Input.get_axis("up", "down");
 	velocity = Vector2(x,y).normalized();
 	
-	move_and_slide(velocity * SPEED);
+	velocity = move_and_slide(velocity * SPEED);
+
+func take_damage(damage):
+	health -= damage
+	if health <= 0:
+		die();
+
+func die():
+	pass
